b62d525635c04c62572d0be202ec3ad31f28c8ca
Imam-Hasan-0 <ihasan063@gmail.com>
2024-09-25 03:04:44 +0600

    complete calculation

--- JS/index.js
+++ JS/index.js
@@ -11,26 +11,42 @@ function activeButton(button) {
   const historyButton = document.getElementById('history-btn');
 
   if (button === 'donation-btn') {
-      donationContainer.classList.add('bg-[#B4F461]');
-      historyContainer.classList.remove('bg-[#B4F461]');
-      donationButton.classList.add('text-white'); 
-      historyButton.classList.remove('text-white');
+    donationContainer.classList.add('bg-[#B4F461]');
+    historyContainer.classList.remove('bg-[#B4F461]');
+    donationButton.classList.add('text-white');
+    historyButton.classList.remove('text-white');
 
   } else if (button === 'history-btn') {
-      historyContainer.classList.add('bg-[#B4F461]');
-      donationContainer.classList.remove('bg-[#B4F461]');
+    historyContainer.classList.add('bg-[#B4F461]');
+    donationContainer.classList.remove('bg-[#B4F461]');
 
-      historyButton.classList.add('text-white');
-      donationButton.classList.remove('text-white');
+    historyButton.classList.add('text-white');
+    donationButton.classList.remove('text-white');
   }
 }
 
-document.getElementById('donation-btn').addEventListener('click', function() {
+document.getElementById('donation-btn').addEventListener('click', function () {
   activeButton('donation-btn');
 });
 
-document.getElementById('history-btn').addEventListener('click', function() {
+document.getElementById('history-btn').addEventListener('click', function () {
   activeButton('history-btn');
 });
 
 // calculate math
+
+document.getElementById('btn-add-money').addEventListener('click', function () {
+  const addMoneyInput = document.getElementById('input-add-money').value;
+  const sumDonation = document.getElementById('total-donation').innerText
+  const addMoneyNumber = parseInt(addMoneyInput)
+  const sumNumber = parseInt(sumDonation)
+  const newDonationBalance = sumNumber + addMoneyNumber;
+  const myBalance = document.getElementById('avilableBalance').innerText
+  const myBalanceNumber = parseInt(myBalance)
+  const myNewBalance = myBalanceNumber - addMoneyNumber
+  document.getElementById('total-donation').innerText = newDonationBalance
+  document.getElementById('avilableBalance').innerText = myNewBalance
+
+  console.log(newDonationBalance)
+})
+
