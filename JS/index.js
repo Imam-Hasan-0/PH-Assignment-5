6ff1309c0491b3da43f4adb43dc45c107398d783
Imam-Hasan-0 <ihasan063@gmail.com>
2024-09-25 22:22:13 +0600

    complete history and add alert

--- JS/index.js
+++ JS/index.js
@@ -4,34 +4,57 @@ function myFunction() {
 }
 
 // btn active
-function activeButton(button) {
-  const donationContainer = document.getElementById('donation-div');
-  const donationButton = document.getElementById('donation-btn');
-  const historyContainer = document.getElementById('history-div');
-  const historyButton = document.getElementById('history-btn');
+// function activeButton(button) {
+//   const donationContainer = document.getElementById('donation-div');
+//   const donationButton = document.getElementById('donation-btn');
+//   const historyContainer = document.getElementById('history-div');
+//   const historyButton = document.getElementById('history-btn');
 
-  if (button === 'donation-btn') {
-    donationContainer.classList.add('bg-[#B4F461]');
-    historyContainer.classList.remove('bg-[#B4F461]');
-    donationButton.classList.add('text-white');
-    historyButton.classList.remove('text-white');
+//   if (button === 'donation-btn') {
+//     donationContainer.classList.add('bg-[#B4F461]');
+//     historyContainer.classList.remove('bg-[#B4F461]');
+//     donationButton.classList.add('text-white');
+//     historyButton.classList.remove('text-white');
 
-  } else if (button === 'history-btn') {
-    historyContainer.classList.add('bg-[#B4F461]');
-    donationContainer.classList.remove('bg-[#B4F461]');
+//   } else if (button === 'history-btn') {
+//     historyContainer.classList.add('bg-[#B4F461]');
+//     donationContainer.classList.remove('bg-[#B4F461]');
 
-    historyButton.classList.add('text-white');
-    donationButton.classList.remove('text-white');
-  }
-}
+//     historyButton.classList.add('text-white');
+//     donationButton.classList.remove('text-white');
+//   }
+// }
 
-document.getElementById('donation-btn').addEventListener('click', function () {
-  activeButton('donation-btn');
-});
+// document.getElementById('donation-btn').addEventListener('click', function () {
+//   activeButton('donation-btn');
+// });
 
-document.getElementById('history-btn').addEventListener('click', function () {
-  activeButton('history-btn');
-});
+// document.getElementById('history-btn').addEventListener('click', function () {
+//   activeButton('history-btn');
+// });
+
+const historyTab = document.getElementById('history-btn')
+const donationTab = document.getElementById('donation-btn')
+// *************   Click History button
+historyTab.addEventListener('click', function () {
+  console.log('hasan')
+  historyTab.classList.add("text-xl", "font-semibold", "bg-[#B4F461]", "rounded-lg", "px-6", "py-3")
+
+  donationTab.classList.remove("text-xl", "font-semibold", "bg-[#B4F461]", "rounded-lg", "px-6", "py-3")
+
+  document.getElementById('all-card').classList.add("hidden");
+  document.getElementById('transaction-section').classList.remove('hidden')
+})
+// *************   Click Donation button
+donationTab.addEventListener('click', function(){
+  console.log('hsds');
+  donationTab.classList.add("text-xl", "font-semibold", "bg-[#B4F461]", "rounded-lg", "px-6", "py-3")
+
+  historyTab.classList.remove("text-xl", "font-semibold", "bg-[#B4F461]", "rounded-lg", "px-6", "py-3")
+
+  document.getElementById('all-card').classList.remove("hidden");
+  document.getElementById('transaction-section').classList.add('hidden')
+})
 
 // calculate math
 
@@ -40,13 +63,30 @@ document.getElementById('btn-add-money').addEventListener('click', function () {
   const sumDonation = document.getElementById('total-donation').innerText
   const addMoneyNumber = parseInt(addMoneyInput)
   const sumNumber = parseInt(sumDonation)
-  const newDonationBalance = sumNumber + addMoneyNumber;
+  
   const myBalance = document.getElementById('avilableBalance').innerText
   const myBalanceNumber = parseInt(myBalance)
+
+  if(isNaN(addMoneyNumber)) {
+    alert('Invalid Donation Amount')
+    return
+  }
+
+  if(addMoneyNumber > myBalanceNumber){
+    alert('Insufficient Balance')
+    return
+  }
+
+  const newDonationBalance = sumNumber + addMoneyNumber;
   const myNewBalance = myBalanceNumber - addMoneyNumber
   document.getElementById('total-donation').innerText = newDonationBalance
   document.getElementById('avilableBalance').innerText = myNewBalance
 
-  console.log(newDonationBalance)
+  const p = document.createElement('p')
+  p.classList.add('w-10/12', 'mx-auto', 'text-3xl', 'font-bold', 'border-2', 'p-3', 'my-5')
+  p.innerText = `
+  ${addMoneyNumber} Taka Donate for Flood at Noakhali, Bangladesh
+  `;
+  document.getElementById('transaction-container').appendChild(p)
 })
 
